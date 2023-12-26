d0a3a68d1a1faab169433e08f004440ef9ec5992
princid <princid85@gmail.com>
2023-12-26 18:54:30 +0530

    added nodemailer

--- app.js
+++ app.js
@@ -58,27 +58,93 @@ const fs = require("fs");
 //   console.log("On PORT : 3000");
 // });
 
+// // my own asynchronous function
+// function practo(){
+//     console.log("inside practo");
+//     return new Promise(function(resolve){
+//         console.log("inside Promise");
+//         fs.readFile("read.txt", "utf8", (err, data) => {
+//           if (err) {
+//             console.error("Error reading File", err);
+//           }
+//           console.log("before resolve");
+//           resolve(data);
+//         });
+//     })
+// }
 
-// my own asynchronous function
-function practo(){
-    console.log("inside practo");
-    return new Promise(function(resolve){
-        console.log("inside Promise");
-        fs.readFile("read.txt", "utf8", (err, data) => {
-          if (err) {
-            console.error("Error reading File", err);
-          }
-          console.log("before resolve");
-          resolve(data);
-        });
-    })
-}
+// // callback function to call
+// function onDone(data){
+//     console.log(data);
+// }
 
+// var a = practo();
+// a.then(onDone);
 
-// callback function to call
-function onDone(data){
-    console.log(data);
-}
+// function someTimeConsumingTask() {
+//   // Simulating a time-consuming operation
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("HUHUHUHUHUHUH");
+//       resolve(); // Indicate that the task is done
+//     }, 10000); // Simulating a delay of 1 second
+//   });
+// }
 
-var a = practo();
-a.then(onDone);
+// async function example() {
+//   console.log("Start");
+
+//   // This line is like telling the waiter to prepare your dish
+//   await someTimeConsumingTask();
+
+//   console.log("Task is done, now I can continue");
+// }
+
+// // When you call the function, it works asynchronously
+// example();
+
+// fs.appendFile("mynewfile1.txt", "Hello content 1 huhuhuhu!", function (err) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
+// fs.open("mynewfile3.txt", "w", function (err, file) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
+// // fs.writeFile("mynewfile3.txt", "Hello content luilui!", function (err) {
+// //   if (err) throw err;
+// //   console.log("Saved!");
+// // });
+
+// fs.unlink("mynewfile2.txt", function (err) {
+//   if (err) throw err;
+//   console.log("File deleted!");
+// });
+
+// Practicing Nodemailer
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "princekumarsingh.mind2web@gmail.com",
+    pass: "vcmkpdpxhkkuklta",
+  },
+});
+
+var mailOptions = {
+  from: "princekumarsingh.mind2web@gmail.com", //not so important
+  to: "princid85@gmail.com",
+  subject: "Sending Email using Node.js",
+  html: "<h1>hey there...</h1><p>Get the Job done!</p>", //more priority than text
+  text: "That was easy! Trying to send mail with nodemailer...",
+  // This prioritization is based on the assumption that HTML provides more formatting options and allows for a richer presentation compared to plain text.
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
